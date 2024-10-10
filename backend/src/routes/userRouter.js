440e8f327b49eecd68511c284b61b1c4c178ba03
matheus <matheusheimann.git@gmail.com>
2024-10-10 17:33:22 -0300

    Documentação Swagger

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -1,8 +1,57 @@
 const router = require('express').Router();
 const { storeUser, loginUser, saveHighScore } = require('../controller/userController');
 
+/**
+ *@swagger
+ * /api/register:
+ *  get: 
+ *    summary: cadastra um novo usuario 
+ *    responses:
+ *      200
+ *         description: Cadastra um novo usuario
+ *         content:
+ *             application/json:
+ *                 schema:
+ *                     type: array  
+ *                     items:
+ *                         type: object
+ */
+
 router.post('/register', storeUser);
+
+/**
+ *@swagger
+ * /api/login:
+ *  get: 
+ *    summary: Login de um usuario 
+ *    responses:
+ *      200
+ *         description: Login de um usuario
+ *         content:
+ *             application/json:
+ *                 schema:
+ *                     type: array  
+ *                     items:
+ *                         type: object
+ */
+
 router.post('/login', loginUser); // Nova rota para login
+
+/**
+ *@swagger
+ * /api/save_highscore:
+ *  get: 
+ *    summary: salva o highscore do usuario
+ *    responses:
+ *      200
+ *         description: salva o highscore do usuario
+ *         content:
+ *             application/json:
+ *                 schema:
+ *                     type: array  
+ *                     items:
+ *                         type: object
+ */
 router.post('/save_highscore', saveHighScore) // Rota pra salvar o highscore
 
 module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,22 @@
 const app = require('./app');
 
 const PORT = app.get('port');
+const swaggerUi = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title:"API de tarefas",
+            version: "1.0.0",
+            description: "API CRUD para gereniar tarefas",
+        },
+        servers: [{url: "http://localhost:3006"}],
+    },
+    apis: [`${__dirname}/routes/*.js`],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 app.listen(PORT, () => console.log(`Running at port ${PORT}`));
